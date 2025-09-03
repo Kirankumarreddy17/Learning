59dfe68e22fbab69708f89a7a073e3ce1fbb67c5
Kirankumarreddy17 <reddykirankumar588@gmail.com>
2025-09-03 20:56:14 +0530

    Add files via upload

---
+++ AOP/src/main/java/com/aop/demo/BeanConfig.java
@@ -0,0 +1,14 @@
+package com.aop.demo;
+
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy
+@Configuration
+@ComponentScan(basePackages = "com.aop.demo")
+public class BeanConfig {
+
+}
---
+++ AOP/src/main/java/com/aop/demo/ShoppingCart.java
@@ -0,0 +1,13 @@
+package com.aop.demo;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShoppingCart {
+	public String checkout(String cart) {
+		System.out.println(cart);
+		System.out.println("Checkout method from shoppingCart class");
+		return "My Order is :- "+ cart;
+	}
+}
