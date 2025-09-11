b7d4de51f53ebfcf672e75fe5596c1a16929330a
Kirankumarreddy17 <reddykirankumar588@gmail.com>
2025-09-11 18:18:02 +0530

    Add files via upload

---
+++ ConnectivityProject/src/com/wipro/service/StudentDaoImp.java
@@ -0,0 +1,11 @@
+package com.wipro.service;
+import java.sql.Connection;
+
+
+
+
+import com.wipro.util.DBConnUtil;
+
+  public class StudentDaoImp{
+	  Connection conn = DBConnUtil.getConn();
+  }
---
+++ ConnectivityProject/src/com/wipro/studentmanagement/StudentApp.java
@@ -0,0 +1,20 @@
+package com.wipro.studentmanagement;
+
+import java.util.Scanner;
+
+import com.wipro.service.StudentDaoImp;
+
+
+
+
+
+public class StudentApp {
+
+	public static void main(String[] args) {
+		
+		StudentDaoImp stud = new StudentDaoImp();
+		System.out.println("Connection established");
+	
+	}
+
+}
