731c8d03d03be1b4c74cc8f6d1fb26e86cc7199e
Kirankumarreddy17 <reddykirankumar588@gmail.com>
2025-09-12 08:57:47 +0530

    Add files via upload

---
+++ HiberORM/src/main/java/com/wipro/HiberORM/App.java
@@ -0,0 +1,13 @@
+package com.wipro.HiberORM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ HiberORM/src/main/java/com/wipro/HiberORM/HQLDemo.java
@@ -0,0 +1,41 @@
+package com.wipro.HiberORM;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import com.wipro.HiberORM.model.Employee;
+import com.wipro.HiberORM.util.HibernateUtil;
+
+public class HQLDemo {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        // CREATE the employee of bharath % surya
+        session.save(new Employee("bharath", 70000));
+        session.save(new Employee("surya", 80000));
+
+        // READ  all the employees
+        Query<Employee> query = session.createQuery("from Employee", Employee.class);
+        List<Employee> employees = query.getResultList();
+        System.out.println("All Employees: " + employees);
+
+        // UPDATE the salary of employee bharath to 90000.0
+        Query updateQuery = session.createQuery("update Employee set salary = :sal where name = :name");
+        updateQuery.setParameter("sal", 90000.0);
+        updateQuery.setParameter("name", "bharath");
+        updateQuery.executeUpdate();
+
+        // DELETE the name of the employee  surya
+        Query deleteQuery = session.createQuery("delete from Employee where name = :name");
+        deleteQuery.setParameter("name", "surya");
+        deleteQuery.executeUpdate();
+
+        tx.commit();
+        session.close();
+        System.out.println("session is closed");
+    }
+}
