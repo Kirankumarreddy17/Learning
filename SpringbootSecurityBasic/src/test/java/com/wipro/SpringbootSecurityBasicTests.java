6c263542d60140ab7a7249931ab92ddee758036e
Kirankumarreddy17 <reddykirankumar588@gmail.com>
2025-09-12 09:11:34 +0530

    Add files via upload

---
+++ SpringbootSecurityBasic/src/test/java/com/wipro/SpringbootSecurityBasicTests.java
@@ -0,0 +1,15 @@
+package com.wipro;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
