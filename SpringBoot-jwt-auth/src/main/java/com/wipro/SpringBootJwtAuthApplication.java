b2b3c5e6a0c1189ca1f105dac992c821369f1858
Kirankumarreddy17 <reddykirankumar588@gmail.com>
2025-09-12 09:12:41 +0530

    Add files via upload

---
+++ SpringBoot-jwt-auth/src/main/java/com/wipro/SpringBootJwtAuthApplication.java
@@ -0,0 +1,14 @@
+package com.wipro;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtAuthApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-jwt-auth/src/main/java/com/wipro/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.wipro.repository;
+
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wipro.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
