fbc9524068028a1339f907f21be8544d56e80779
Kirankumarreddy17 <reddykirankumar588@gmail.com>
2025-09-12 08:55:37 +0530

    Add files via upload

---
+++ hibernate-onetomany-mapping/src/main/java/com/wipro/App.java
@@ -0,0 +1,53 @@
+package com.wipro;
+
+/**
+ * Hello world!
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.wipro.model.Course;
+import com.wipro.model.ReportCard;
+import com.wipro.model.Student;
+import com.wipro.util.HibernateUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	// to obtain/get the session from sessionfactory (It can be multiple session based on multiple services or work)
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		
+		// To create a  transaction ( Transient + Persistent) -- Transient when you do not to reflect and save in backend  but persistent to save into a backend
+	   Transaction tx = session.beginTransaction();
+	   
+	   Student s =  new Student("Niti");
+	   
+	   s.addCourse(new Course("Java"));
+	   s.addCourse(new Course("PD"));
+	   
+	   ReportCard rc = new ReportCard(450);     
+	   s.setReportcard(rc);
+	   	   
+	   session.persist(s);
+	   
+	   tx.commit();
+	   
+	   session.close();
+	   
+	   Session getSession = HibernateUtil.getSessionFactory().openSession();
+	   Student stud = getSession.get(Student.class,1L);
+	   System.out.println("Student Details :" + s.getName());
+	   s.getCourses().forEach(c->System.out.println("Courses enrolled for : "+ c.getTitle()));
+	   getSession.close(); 
+    }
+}
