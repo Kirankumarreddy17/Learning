e186825f2148aa0f541c69939efc338cd17ebcff
Kirankumarreddy17 <reddykirankumar588@gmail.com>
2025-09-12 08:57:15 +0530

    Add files via upload

---
+++ HibernateORMTask/src/main/java/com/wipro/HibernateORMTask/AppNativeSQL.java
@@ -0,0 +1,43 @@
+package com.wipro.HibernateORMTask;
+
+
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.NativeQuery;
+
+import com.wipro.HibernateORMTask.model.Employee;
+import com.wipro.HibernateORMTask.util.HibernateUtil;
+
+public class AppNativeSQL {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        // CREATE 
+        session.save(new Employee("kiran", 50000));
+        session.save(new Employee("jashu", 60000));
+
+        // READ all the employess in the table
+        NativeQuery<Employee> query = session.createNativeQuery("SELECT * FROM employee", Employee.class);
+        List<Employee> employees = query.getResultList();
+        System.out.println("All Employees: " + employees);
+
+        // UPDATE the salary for the employee name jashu to 75000.0
+        session.createNativeQuery("UPDATE employee SET salary = :sal WHERE name = :name")
+                .setParameter("sal", 75000.0)
+                .setParameter("name", "jashu")
+                .executeUpdate();
+
+        // DELETE the employee name with kiran
+        session.createNativeQuery("DELETE FROM employee WHERE name = :name")
+                .setParameter("name", "kiran")
+                .executeUpdate();
+
+        tx.commit();
+        session.close();
+        System.out.println("Session is closed");
+    }
+}
---
+++ HibernateORMTask/src/main/java/com/wipro/HibernateORMTask/util/HibernateUtil.java
@@ -0,0 +1,28 @@
+package com.wipro.HibernateORMTask.util;
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import com.wipro.HibernateORMTask.model.Employee;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration().configure("hibernateTask.xml");
+            configuration.addAnnotatedClass(Employee.class);
+            StandardServiceRegistryBuilder builder =
+                    new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+            sessionFactory = configuration.buildSessionFactory(builder.build());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
